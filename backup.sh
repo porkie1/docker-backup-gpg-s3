fdfb2c1aa5ec1cd077220979bb7601f18791d3d3
porkie1 <58346822+porkie1@users.noreply.github.com>
2020-12-25 22:00:32 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ cd /backup
 tar cJf ~/$BACKUP_NAME.tar.xz ./*
 cd /
 
-gpg --trust-model always --output ~/$BACKUP_NAME.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$BACKUP_NAMEtar.xz
+gpg --trust-model always --output ~/$BACKUP_NAME.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$BACKUP_NAME.tar.xz
 rm ~/$BACKUP_NAME.tar.xz
 
 aws s3 cp ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg s3://$S3_BUCKET_NAME/$BACKUP_NAME.tar.xz.gpg --storage-class STANDARD --region $AWS_DEFAULT_REGION
