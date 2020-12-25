6ca14b389839e479baa83aa3045b152f371d36a8
porkie1 <58346822+porkie1@users.noreply.github.com>
2020-12-25 20:50:13 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,5 +9,5 @@ cd /
 gpg --trust-model always --output ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --encrypt --recipient $GPG_RECIPIENT ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
 rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
 
-aws s3 cp ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg s3://$S3_BUCKET_NAME/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --storage-class STANDARD_IA
+aws s3 cp ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg s3://$S3_BUCKET_NAME/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --storage-class STANDARD_IA --region $AWS_DEFAULT_REGION
 rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg
